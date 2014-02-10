e7287fee460baa9f77842e5e0ca7c2a63d1948f4
Brad Hubbard <brad.hubbard77@gmail.com>
2014-02-10 11:15:37 -0600

    add initialize method to Pizza class

--- pizza.rb
+++ pizza.rb
@@ -1,9 +1,14 @@
 class Pizza
+  attr_accessor :toppings
+  def initialize(toppings)
+    @toppings = toppings
+  end
 end
 
 class Topping
-  attr_accessor :name
-  def initialize(name)
+  attr_accessor :name, :vegetarian
+  def initialize(name, vegetarian: false)
     @name = name
+    @vegetarian = vegetarian
   end
 end
